a3b4e8f5017ed45ebb1429ce175c7bae2394c674
CARLOS CARVAJAL <u1424676@lab1-33.eng.utah.edu>
2023-10-01 13:19:12 -0600

    modified envs and main.yml. crearranged directories for tidiness

--- test/test_signal/test_signal.c
+++ test/test_signal/test_signal.c
@@ -139,19 +139,3 @@ int main (void)
     RUN_TEST(test_noone_home);
     return UNITY_END();
 }
-
-void signal_handle_calculation(struct k_sem *request, struct k_sem *response, struct signal_data *data)
-{
-
-    k_sem_take(request, K_FOREVER);
-    data->output = data->input + 5;
-    k_sleep(K_MSEC(1));
-    k_sem_give(response);
-}
-
-int signal_request_calculate(struct k_sem *request, struct k_sem *response, struct signal_data *data)
-{
-    k_sem_give(request);
-    int res = k_sem_take(response, K_MSEC(1000));
-    return res;
-}
